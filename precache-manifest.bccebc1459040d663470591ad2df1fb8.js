d224dae254cf809de6ce45a4afdfcb798da45cc4
Ben Hanna <benpaulhanna@gmail.com>
2019-09-05 14:26:08 -0400

    Updates

---
+++ precache-manifest.bccebc1459040d663470591ad2df1fb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e594842291f35169ba7a2c2874f8d14f",
+    "url": "/react-svg-drag-and-drop/index.html"
+  },
+  {
+    "revision": "2a3f272ded54b4f7010b",
+    "url": "/react-svg-drag-and-drop/static/css/2.53dc0e97.chunk.css"
+  },
+  {
+    "revision": "f22942c2e6db8b7f126e",
+    "url": "/react-svg-drag-and-drop/static/css/main.869e4a8f.chunk.css"
+  },
+  {
+    "revision": "2a3f272ded54b4f7010b",
+    "url": "/react-svg-drag-and-drop/static/js/2.7300e902.chunk.js"
+  },
+  {
+    "revision": "f22942c2e6db8b7f126e",
+    "url": "/react-svg-drag-and-drop/static/js/main.7b5f2c8a.chunk.js"
+  },
+  {
+    "revision": "215171422d52ae852b83",
+    "url": "/react-svg-drag-and-drop/static/js/runtime~main.fec8cd62.js"
+  }
+]);
